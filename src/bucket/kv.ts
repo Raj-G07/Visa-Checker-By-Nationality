c71f9bd4d47b3fdb3324b9781dfb215c2aae48fa
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-12-31 19:54:47 +0530

    Remove duplicate

--- src/bucket/kv.ts
+++
@@ -1,9 +0,0 @@
-import { Actor } from 'apify';
-
-export const storeKVResult = async (result: any) => {
-  await Actor.setValue('RESULT', result);
-};
-
-export const storeHTMLReport = async (html: string) => {
-  await Actor.setValue('report.html', html, { contentType: 'text/html' });
-};
